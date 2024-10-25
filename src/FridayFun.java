9765acceedf7e9a1e93edb1e6e6ef408b6655cef
Quincy Holmer <qholmer@schools.nyc.gov>
2024-10-25 12:52:26 -0400

    added some print statements

--- src/FridayFun.java
+++ src/FridayFun.java
@@ -1,5 +1,8 @@
 public class FridayFun {
     public static void main(String[] args) {
         System.out.println("its friday");
+        System.out.println("hello world!");
+        System.out.println("tomorrow is sat");
+        System.out.println("yay candy");
     }
 }
